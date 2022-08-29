c19d59e75603031aad4a63282ad190e93cb3d728
Dan Sheinin <dansheinin@gmail.com>
2022-08-29 21:20:26 +0300

    have basic functionality - depending on amount of xp user have

--- Scripts/Player.lua
+++ Scripts/Player.lua
@@ -1,8 +1,3 @@
 dofile "$SURVIVAL_DATA/Scripts/game/SurvivalPlayer.lua"
 
-Player = class( SurvivalPlayer )
-
-function Player.sv_addKill( self, player ) 
-    print("Kill")
-    sm.gui.displayAlertText( "Kill!", 5 )
-end
+SurvivalPlayer = class( SurvivalPlayer )
