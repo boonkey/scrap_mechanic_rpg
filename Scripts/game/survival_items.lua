74510ad54d2339639fd0dc18c708350b95096dd2
Dan Sheinin <dansheinin@gmail.com>
2022-09-01 15:28:10 +0300

    still lots of bugs

--- Scripts/game/survival_items.lua
+++ Scripts/game/survival_items.lua
@@ -1,5 +1,7 @@
 obj_experience = sm.uuid.new( "73d57575-8941-425f-a7e8-caafb6d9b7e9" )
 
+obj_resource_electronics = sm.uuid.new( "404baa74-2b2c-4577-b96f-27b107b9430e" )
+
 obj_craftbot_craftbot1_t = sm.uuid.new( "1fe1ae71-9353-4095-9f1c-fd51de309a68" )
 obj_craftbot_craftbot2_t = sm.uuid.new( "5951c416-a459-4a13-8c37-cf2128752a06" )
 obj_craftbot_craftbot3_t = sm.uuid.new( "663304e2-e1e1-43f1-ba7f-676f57ad1600" )
